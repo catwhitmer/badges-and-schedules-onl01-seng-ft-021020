3549eed7da795a5a6d35f7a02b8caf974467105a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-12 18:43:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ list_of_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"
 
 def batch_badge_creator(list_of_speakers)
   list_of_speakers.each do |attendees|
-    puts "Hello, my name is #{list_of_speakers"
+    puts "Hello, my name is #{list_of_speakers."
   end
 end
 

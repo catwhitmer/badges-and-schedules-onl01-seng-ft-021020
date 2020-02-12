c1751fab030a7472ef8318af520cd48201be7f6e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-12 18:32:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,12 @@ end
 
 list_of_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(names)
-  puts "Hello, my name is #{names}."
+def batch_badge_creator(array_of_names)
+  
 end
 
 def assign_rooms(list_of_speakers)
-  
+  list_of_speakers.each do |names|
+    puts "Hello, #names}! You'll be assigned to room !"
+  end
 end

dc5f446f0d6b11aa7ff8c3d7abae7d8fafcf66d5
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-12 19:03:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,7 @@ def assign_rooms(attendees)
   attendees.each do |room.each_with_index|
     room_assignments.push("Hello, #{attendees}! You'll be assigned to room ${room}!")
   end
+  return room_assignments
 end
 
 #def printer()
